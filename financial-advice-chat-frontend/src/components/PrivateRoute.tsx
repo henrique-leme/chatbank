6be492d497e41670635c925ef097ab0d791a35d3
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-09 14:47:39 -0300

    feat: auth model

---
+++ financial-advice-chat-frontend/src/components/PrivateRoute.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const PrivateRoute: React.FC<{ component: React.FC<any>; path: string }> = ({
+  component: Component,
+  ...rest
+}) => {
+  const { currentUser } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return currentUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
