f0aa9625476a9a76161cbc92005d0d4b2f6cda85
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-12 09:51:31 -0300

    feat: index backend

--- financial-advice-chat-backend/src/index.ts
+++ financial-advice-chat-backend/src/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import chatRoutes from "./routes/chatRoutes";
+import scrapingRoutes from "./routes/scrapingRoutes";
+import http from "http";
+import cors from "cors";
 
 const app = express();
-
 app.use(express.json());
 
-app.use("/user", userRoutes);
+app.use(cors());
+
+app.use("/users", userRoutes);
 app.use("/chat", chatRoutes);
+app.use("/scraping", scrapingRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+
+server.setTimeout(300000);
+
+server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
