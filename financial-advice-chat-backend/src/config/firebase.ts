3b90918faf1dcb8baeeea2ada47a1db6eba33956
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-12 09:51:49 -0300

    feat: firebase config

--- financial-advice-chat-backend/src/config/firebase.ts
+++ financial-advice-chat-backend/src/config/firebase.ts
@@ -1,12 +1,9 @@
 import * as admin from "firebase-admin";
+import * as serviceAccount from "../config/adminsdk.json";
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  }),
-  databaseURL: "https://your-database-name.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: "https://financial-advice-chat-default-rtdb.firebaseio.com",
 });
 
 export const db = admin.firestore();
