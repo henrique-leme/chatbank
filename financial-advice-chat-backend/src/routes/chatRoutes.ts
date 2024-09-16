c9ac59dcb0193be15cefaeab19e0ca57589bb583
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-16 09:53:28 -0300

    feat: chat routes

--- financial-advice-chat-backend/src/routes/chatRoutes.ts
+++ financial-advice-chat-backend/src/routes/chatRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getChatHistory, addChatMessage } from "../controllers/chatController";
+import {
+  getChatHistory,
+  getFinancialLevelQuestions,
+  addChatMessageFinance,
+} from "../controllers/chatController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/history", authMiddleware, getChatHistory);
-router.post("/message", authMiddleware, addChatMessage);
+router.get("/questions/llama", authMiddleware, getFinancialLevelQuestions);
+router.post("/message/finance", authMiddleware, addChatMessageFinance);
 
 export default router;
