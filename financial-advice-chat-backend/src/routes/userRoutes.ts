1bc5bfd58eeb7e3c39f167459cfa2802525e8e1f
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-17 09:53:47 -0300

    feat: user routes

--- financial-advice-chat-backend/src/routes/userRoutes.ts
+++ financial-advice-chat-backend/src/routes/userRoutes.ts
@@ -2,12 +2,28 @@ import { Router } from "express";
 import {
   getUserProfile,
   updateUserProfile,
+  createUserProfile,
+  getAllUserProfiles,
+  deleteUserProfile,
+  findUserByEmail,
+  evaluateFinancialLevel,
 } from "../controllers/userController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { loginUser } from "../controllers/authController";
 
 const router = Router();
 
-router.get("/profile", authMiddleware, getUserProfile);
-router.put("/profile", authMiddleware, updateUserProfile);
+router.post("/register", createUserProfile);
+router.post("/login", loginUser);
+router.get("/details", authMiddleware, getUserProfile);
+router.put("/details", authMiddleware, updateUserProfile);
+router.get("/", authMiddleware, getAllUserProfiles);
+router.post("/search", authMiddleware, findUserByEmail);
+router.delete("/delete", authMiddleware, deleteUserProfile);
+router.post(
+  "/evaluate-finance-level/llama",
+  authMiddleware,
+  evaluateFinancialLevel
+);
 
 export default router;
