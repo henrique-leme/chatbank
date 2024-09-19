a1dbeaa7ffbd7d7c08b4976a5c579849b5548321
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-09-19 09:54:42 -0300

    feat: service auth

--- financial-advice-chat-backend/src/services/authService.ts
+++ financial-advice-chat-backend/src/services/authService.ts
@@ -1,22 +1,13 @@
-import admin from "firebase-admin";
-import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
 
-export const verifyToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const token = req.headers.authorization?.split(" ")[1];
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
 
-  if (!token) {
-    return res.status(401).send("Token não encontrado.");
-  }
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    res.locals.user = decodedToken;
-    next();
-  } catch (error) {
-    return res.status(403).send("Token inválido.");
-  }
+export const comparePassword = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
 };
