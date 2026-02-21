3a95341f93dacfc07f8df383146d25dfc672f367
henriqueleme.brito@outlook.com <henriqueleme.brito@outlook.com>
2026-02-21 14:08:49 -0300

    chore: update deploy script to explicitly use bash for improved compatibility

--- infra/deploy-conversafina.sh
+++ infra/deploy-conversafina.sh
@@ -4,9 +4,9 @@ set -e
 echo "Inciando script de deploy..."
 
 echo "Iniciando o build e push das imagens para o ecr..."
-./push-frontend-image-to-ecr.sh
-./push-backend-image-to-ecr.sh
-./push-nginx-image-to-ecr.sh
+bash push-frontend-image-to-ecr.sh
+bash push-backend-image-to-ecr.sh
+bash push-nginx-image-to-ecr.sh
 
 echo "Iniciando o deploy dos containers para o ecs..."
 terraform -chdir=terraform apply -auto-approve
